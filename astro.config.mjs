b0746bb3b249a9bc5beed60a274bbbb91b18eff8
clementthee <clementthee@gmail.com>
2026-05-16 19:57:29 -0600

    Update astro.config.mjs configuration
    Removed commented lines and updated site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Reemplaza con tu usuario de GitHub y el nombre exacto de tu repositorio
   site: 'https://clementthee.github.io',
-  base: '/mi-proyecto', 
   vite: {
     plugins: [tailwindcss()],
   },
